012e3a617cc362fd5a7d56ed8b1a95cf0443b4af
Elle Valentine <elle.valentine@hotmail.com>
2019-03-29 12:24:51 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,10 +2,24 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+min_value = 0 
 key_for_min_value.collect do |key, value|
-     key
+     
  end
+end
 
 
+def big_shoe_rebounds
+  biggest = 0
+  rebounds = 0
+  game_hash.each do |home_away, keys|
+    keys[:players].each do |player|
+      size = player[:shoe]
+      if size > biggest
+        biggest = size
+        rebounds = player[:rebounds]
+      end
+    end
+  end
+  rebounds
 end
